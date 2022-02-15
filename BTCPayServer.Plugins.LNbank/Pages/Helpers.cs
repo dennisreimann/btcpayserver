0580e5bf9b0ce3342f2c1357cd20989238b14654
Dennis Reimann <mail@dennisreimann.de>
2022-02-15 13:21:44 +0100

    Add helper for sats values

---
+++ BTCPayServer.Plugins.LNbank/Pages/Helpers.cs
@@ -0,0 +1,9 @@
+using System;
+using BTCPayServer.Lightning;
+
+namespace BTCPayServer.Plugins.LNbank.Pages;
+
+public static class Helpers
+{
+    public static string Sats(LightMoney amount) => $"{Math.Round(amount.ToUnit(LightMoneyUnit.Satoshi))} sats";
+}
